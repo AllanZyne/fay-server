3784a67787919c94c53036f4e006b2fc81f4a741
Allan Zyne <allanzyne@icloud.com>
2016-09-16 19:02:41 +0800

    Update login

--- public/js/login.js
+++ public/js/login.js
@@ -1,24 +1,17 @@
 
-var bcrypt = dcodeIO.bcrypt;
-
-
-
 $('form').addEventListener('submit', function(event) {
     event.preventDefault();
     console.log('submit');
     let data = new FormData(this),
         username = data.get('username');
         password = data.get('password');
-    // let salt = bcrypt.genSaltSync(10);
-    // let hash = bcrypt.hashSync(password, salt);
+
     getData('http://allan-pc:3000/api/authenticate', {
         username: username,
         password: password
     }).then(data => {
         console.log('[login] data', data);
-        if (data.token) {
-            window.location.search = `?token=${data.token}`;
-        } else {
+        if (data) {
             if (data.message.match('username')) {
                 $('#username').classList.add('invalid');
                 $('#password').classList.remove('invalid');
@@ -26,6 +19,8 @@ $('form').addEventListener('submit', function(event) {
                 $('#password').classList.add('invalid');
                 $('#username').classList.remove('invalid');
             }
+        } else {
+            window.location = '/';
         }
     }).catch(err => console.log('[login] error', err));
 });
