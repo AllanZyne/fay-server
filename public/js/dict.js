5e92b92bc91de3a4b618cf0ed9f551e971aec017
Allan Zyne <allanzyne@icloud.com>
2016-10-01 14:17:05 +0800

    Add files

---
+++ public/js/dict.js
@@ -0,0 +1,9 @@
+
+
+document.addEventListener('mouseup', function(){
+    let thetext = window.getSelection().toString();
+    if (thetext.length > 0) {
+        // console.log(thetext);
+    }
+}, false);
+
