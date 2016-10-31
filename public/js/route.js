2019359933da817606317005567bb7f034788043
Allan Zyne <allanzyne@outlook.com>
2016-10-31 17:33:51 +0800

    xxx

---
+++ public/js/route.js
@@ -0,0 +1,25 @@
+"use strict";
+
+
+
+var Router = {
+	urls: [],
+	init() {
+		window.addEventListener("load", this._checkURL);
+		window.addEventListener("popstate", this._checkURL);
+	},
+	bind(url, callback) {
+
+	},
+	go(url) {
+
+	},
+	back() {
+
+	},
+	_checkURL {
+
+	}
+};
+
+
